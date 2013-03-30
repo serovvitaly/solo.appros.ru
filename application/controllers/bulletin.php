26e60f48667dd7ab1d7940eeddaaaf4375570e8e
Vitaly Serov <serovvitaly@gmail.com>
2013-03-30 15:21:17 +0400

    база объявлений

--- application/controllers/bulletin.php
+++ application/controllers/bulletin.php
@@ -15,7 +15,7 @@ class Bulletin_Controller extends Base_Controller {
 
     public function action_index()
     {
-        $items = Advert::take(20)->get();
+        $items = Advert::where('status', '=', 3)->take(20)->get();
         
         $this->layout->content = View::make('base.bulletin.content', array('items' => $items));
         
