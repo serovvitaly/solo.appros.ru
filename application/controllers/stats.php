872f372014d932942146e18a7a78616c5481e673
Vitaly Serov <serovvitaly@gmail.com>
2013-04-04 01:45:28 +0400

    очередной коммит

--- application/controllers/stats.php
+++ application/controllers/stats.php
@@ -2,11 +2,11 @@
 
 class Stats_Controller extends Base_Controller
 {
-    public $layout = 'base.column2_left';
+    public $layout = 'base.column1';
     
     public function action_index()
     {
-        //
+        $this->layout->content = View::make('base.bulletin.grid');
     }
     
 }
